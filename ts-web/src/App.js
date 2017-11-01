31867c528b7c03249b83420477a4ee536b3f23c6
GiapVo <kinhkyo@gmail.com>
2017-11-01 12:07:41 +0700

    setting project

---
+++ ts-web/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import Main from './scenes/main'
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
