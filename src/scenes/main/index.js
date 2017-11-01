879bef0f650f6a4c58dd9e9e05645a114284e785
GiapVo <kinhkyo@gmail.com>
2017-11-01 16:28:17 +0700

    - Update config

--- src/scenes/main/index.js
+++ src/scenes/main/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Header from '../../layouts/header'
 import { Button } from 'antd';
-import {routes, RouteWithSubRoutes } from '../../services/routes/route.config'
+import { routes, RouteWithSubRoutes } from '../../services/routes/route.config'
 
 
 export default class Main extends Component {
@@ -10,10 +10,10 @@ export default class Main extends Component {
             <main>
                 <Header />
                 <Button type="primary">Button</Button>
-                
+
                 {routes.map((route, i) => (
-                <RouteWithSubRoutes key={i} {...route}/>
-              ))}
+                    <RouteWithSubRoutes key={i} {...route} />
+                ))}
             </main>
         )
     }
