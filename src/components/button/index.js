43dfb16dad37afe556f131ebec3b7bc1bd8cec8f
GiapVo <kinhkyo@gmail.com>
2017-11-05 12:06:01 +0700

    update: button component
    - Change name button to TSButton, accept classNames  from parents

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,31 +2,34 @@ import React, { Component } from 'react'
 import './index.less'
 import classNames from 'classnames';
 
-export default class Button extends Component {
+export default class TSButton extends Component {
     constructor() {
         super()
     }
 
     render() {
+        var cn = this.props.className
         const buttonStyle = {
             color: `${this.props.color}`,
             height: `${this.props.height}px`,
             borderRadius: `${this.props.borderRadius}px`,
+            width: `${this.props.width}px`
 
         };
         var btnClass = classNames({
             'bt-primary': this.props.type === 'primary',
             'bt-outline': this.props.type === 'default',
-        });
+        }, this.props.className);
         return <button className={btnClass} style={buttonStyle}> {this.props.children}</button>
     }
 }
 
 
 
-Button.defaultProps = {
+TSButton.defaultProps = {
     height: '32',
     borderRadius: '100',
     color: 'white',
+    width: '116',
     type: 'default'
 }
