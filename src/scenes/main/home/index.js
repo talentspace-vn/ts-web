92d31aeb3ba8ebcee26f4fe37d4c5a51a36cd870
GiapVo <kinhkyo@gmail.com>
2017-11-06 09:12:22 +0700

    fix: format code with eslint rules

--- src/scenes/main/home/index.js
+++ src/scenes/main/home/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './home.less';
 import SearchSection from './components/search-section';
-import FeatureJobSection from './components/features-job-section'
+import FeatureJobSection from './components/features-job-section';
 
 class Home extends React.Component {
   render() {
