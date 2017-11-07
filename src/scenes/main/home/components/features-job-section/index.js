8fde031580bc1139111d428201d1b0086f0377e9
GiapVo <kinhkyo@gmail.com>
2017-11-07 12:06:44 +0700

    feature: Home page jobs section
    - Create job section
    - Create  job card component

--- src/scenes/main/home/components/features-job-section/index.js
+++ src/scenes/main/home/components/features-job-section/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+
 import './features-job.less';
+import FeatureJobCard from '../../../../../components/feature-job-card';
 
 export default class FeatureJobSection extends Component {
   render() {
@@ -8,6 +11,24 @@ export default class FeatureJobSection extends Component {
         <div>
           <p className="fj-title">FEATURED JOBS</p>
           <div className="line" />
+          <div className="fj-list">
+            <Row className="fj-row">
+              <Col span={10} offset={1}>
+                <FeatureJobCard />
+              </Col>
+              <Col span={10} offset={1}>
+                <FeatureJobCard />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={10} offset={1}>
+                <FeatureJobCard />
+              </Col>
+              <Col span={10} offset={1}>
+                <FeatureJobCard />
+              </Col>
+            </Row>
+          </div>
         </div>
       </section>
     );
