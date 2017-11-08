71d13b92f00b0e67a1776a1225d715516a915207
GiapVo <kinhkyo@gmail.com>
2017-11-08 23:04:38 +0700

    feature: [home page] create apply section

--- src/scenes/main/home/components/apply-section/index.js
+++ src/scenes/main/home/components/apply-section/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Row, Col } from 'antd';
-import TSButton from '../../../../../components/button';
 import './apply-section.less';
+import TSButton from '../../../../../components/button';
 
 export default class ApplySection extends PureComponent {
   render() {
@@ -10,7 +10,7 @@ export default class ApplySection extends PureComponent {
         <Row>
           <Col span={12}>
             <div className="employer">
-              <p className="title">I'M AN EMPLOYER</p>
+              <p className="title">I AM AN EMPLOYER</p>
               <p className="description"> Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus</p>
               <TSButton borderRadius={4} width={169} color="#e94347" type="background-white">Apply</TSButton>
             </div>
@@ -18,7 +18,7 @@ export default class ApplySection extends PureComponent {
           </Col>
           <Col span={12}>
             <div className="candidate">
-              <p className="title">I'M AN CANDIDATE</p>
+              <p className="title">I AM AN CANDIDATE</p>
               <p className="description"> Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus</p>
               <TSButton borderRadius={4} width={169} color="#10223f" type="background-white">Apply</TSButton>
             </div>
